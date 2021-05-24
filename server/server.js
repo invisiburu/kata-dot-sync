7abb7dc890f2cea7fa23873b61f8791fffd29729
Maksym Nesterov <maxym.nes.dev@gmail.com>
2021-05-24 21:57:10 +0300

    Add heroku

--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const express = require('express')
 const sockets = require('./sockets')
 
 const PORT = process.env.PORT
-const HOST = process.env.HOST
+const CORS = process.env.CORS || 'http://127.0.0.1:3000'
 const DOC_ROOT = './../dist/'
 
 const app = express()
@@ -16,8 +16,8 @@ const documentRoot = path.join(__dirname, DOC_ROOT)
 const staticContent = express.static(documentRoot)
 app.use(staticContent)
 
-server.listen(PORT, HOST, () => {
-  console.log(`Server is running on port http://${HOST}:${PORT}`)
+server.listen(PORT, () => {
+  console.log(`Server is running on http://127.0.0.1:${PORT}`)
 })
 
-sockets.init(server, `http://${HOST}:${PORT}`)
+sockets.init(server, CORS)
